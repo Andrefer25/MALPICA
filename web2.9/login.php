9b6357464c8dd7ac99a8a6184c7625b15877920d
André Malpica <32719793+Andrefer25@users.noreply.github.com>
2018-07-23 13:15:20 -0300

    Update login.php

--- web2.9/login.php
+++ web2.9/login.php
@@ -57,14 +57,14 @@
 				</div>
 				<div class="col-sm-6">
 					<h1>¿Ya estás registrado?</h1>
-					<form>
+					<form action="php/ingresar.php" method="POST" id="sesion">
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <label for="exampleInputEmail1">Usuario</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre de Usuario" name="usuario" required>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="exampleInputPassword1">Contraseña</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña" name="clave" required>
                         </div>
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input"id="exampleCheck1">
